e5533f498d0ba70a9077ab308ddfd0715bb3adf1
sokolovry <sokolovry@yandex.ru>
2025-10-28 23:46:41 +0300

    add: доработан функционал для калькулятора (математические операции с числами)

--- bjs/07_Number_and_string/script.js
+++ bjs/07_Number_and_string/script.js
@@ -10,3 +10,96 @@ document.getElementById('btn_clr').addEventListener('click', function () {
     inputWindow.value = '';
 })
 
+document.getElementById('btn_1').addEventListener('click', function () {
+    inputWindow.value += '1';
+})
+
+document.getElementById('btn_2').addEventListener('click', function () {
+    inputWindow.value += '2';
+})
+
+document.getElementById('btn_3').addEventListener('click', function () {
+    inputWindow.value += '3';
+})
+
+document.getElementById('btn_4').addEventListener('click', function () {
+    inputWindow.value +=  '4';
+})
+
+document.getElementById('btn_5').addEventListener('click', function () {
+    inputWindow.value +=  '5';
+})
+
+document.getElementById('btn_6').addEventListener('click', function () {
+    inputWindow.value +=  '6';
+})
+
+document.getElementById('btn_7').addEventListener('click', function () {
+    inputWindow.value +=  '7';
+})
+
+document.getElementById('btn_8').addEventListener('click', function () {
+    inputWindow.value += '8';
+})
+
+document.getElementById('btn_9').addEventListener('click', function () {
+    inputWindow.value +=  '9';
+})
+
+document.getElementById('btn_0').addEventListener('click', function () {
+    inputWindow.value +=  '0';
+})
+
+document.getElementById('btn_add').addEventListener('click', function () {
+    lastOperand = inputWindow.value;
+    operation = '+';
+    inputWindow.value = '';
+})
+
+document.getElementById('btn_diff').addEventListener('click', function () {
+    lastOperand = inputWindow.value;
+    operation = '-';
+    inputWindow.value = '';
+})
+
+document.getElementById('btn_div').addEventListener('click', function () {
+    lastOperand = inputWindow.value;
+    operation = '/';
+    inputWindow.value = '';
+})
+
+document.getElementById('btn_mult').addEventListener('click', function () {
+    lastOperand = inputWindow.value;
+    operation = '*';
+    inputWindow.value = '';
+})
+
+document.getElementById('btn_sqrt').addEventListener('click', function () {
+    lastOperand = inputWindow.value;
+    operation = 'sqrt';
+    inputWindow.value = Math.sqrt(parseInt(lastOperand));
+})
+
+document.getElementById('btn_result').addEventListener('click', function () {
+    let result = 0;
+    switch(operation) {
+        case "+":
+            result = parseInt(lastOperand) + parseInt(inputWindow.value);
+            inputWindow.value = result;
+            break;
+        case "-":
+            result = parseInt(lastOperand) - parseInt(inputWindow.value);
+            inputWindow.value = result;
+            break;   
+        case "*":
+            result = parseInt(lastOperand) * parseInt(inputWindow.value);
+            inputWindow.value = result;
+            break;  
+        case "/":
+            if (inputWindow.value == 0) return;
+            result = parseInt(lastOperand) / parseInt(inputWindow.value);
+            inputWindow.value = result;
+            break;      
+    }
+})
+
